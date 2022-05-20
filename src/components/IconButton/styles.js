e60e6a21ba9806ba1b44bc4a91cc4ef88f7fe724
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-05-20 20:50:48 -0300

    feat: add icon button component

---
+++ src/components/IconButton/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Container = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2px;
+`;
+
+export default Container;
