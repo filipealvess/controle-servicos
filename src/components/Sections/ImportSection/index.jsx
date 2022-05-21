4511662f6e7c40989a940c304f8c1da6f25f5542
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-05-21 16:54:08 -0300

    feat: add effect in import section component

--- src/components/Sections/ImportSection/index.jsx
+++ src/components/Sections/ImportSection/index.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Download } from 'react-feather';
 import Container, { Description, Title } from './styles';
 
 export default function ImportSection() {
+  const [isActive, setIsActive] = useState(false);
+
   return (
-    <Container>
+    <Container
+      isActive={isActive}
+      onDragOver={() => setIsActive(true)}
+      onDragLeave={() => setIsActive(false)}
+    >
       <Download size={64} />
       <Title>Importe os dados de prestadores e serviços</Title>
       <Description>Selecione um arquivo CSV do seu computador</Description>
