93fe0b02edc29b25c002f58dd5dcd7551a36c180
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-05-21 08:38:10 -0300

    feat: add navigation between tabs

--- src/components/Tabs/index.jsx
+++ src/components/Tabs/index.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Container, { Tab } from './styles';
 
 export default function Tabs({ currentPage = 'login' }) {
+  const navigate = useNavigate();
+  const tabs = [
+    { id: 0, name: 'Entrar', page: 'login', route: '/' },
+    { id: 1, name: 'Criar Conta', page: 'signup', route: '/criar-conta' }
+  ];
+
+  function handleTabClick(page, route) {
+    if (currentPage !== page) {
+      navigate(route);
+    }
+  }
+
   return (
     <Container>
-      <Tab active={currentPage === 'login'}>
-        Entrar
-      </Tab>
-
-      <Tab active={currentPage === 'signup'}>
-        Criar Conta
-      </Tab>
+      {
+        tabs.map(({ id, page, name, route }) => (
+          <Tab
+            key={id}
+            active={currentPage === page}
+            onClick={() => handleTabClick(page, route)}
+          >{name}</Tab>
+        ))
+      }
     </Container>
   );
 }
