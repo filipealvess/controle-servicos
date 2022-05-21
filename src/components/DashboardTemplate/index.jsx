4fe7e7f9fbda75e0849bcdaf2b01ae09d9c5740c
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-05-21 10:38:16 -0300

    feat: add dashboard template

---
+++ src/components/DashboardTemplate/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { LogOut } from 'react-feather';
+import dashboardLinks from '../../static/dashboardLinks';
+import Expanded from '../Expanded';
+import PageLink from '../PageLink';
+import Container, { Divider, Image, Links, Logo, Main, Menu, Title } from './styles';
+
+export default function DashboardTemplate({ children, currentPage }) {
+  return (
+    <Container>
+      <Menu>
+        <Logo>
+          <Image
+            src="/images/logo.svg"
+            alt="Logo do Controle de Serviços: chave no centro de um círculo."
+            width={40}
+            height={40}
+          />
+
+          <Title>Controle <br /> de Serviços</Title>
+        </Logo>
+
+        <Divider />
+
+        <Links>
+          {dashboardLinks.map(({ icon, id, page, title, route }) => (
+            <PageLink
+              key={id}
+              icon={icon}
+              title={title}
+              isActive={currentPage === page}
+              route={route}
+            />
+          ))}
+
+          <Expanded />
+
+          <PageLink icon={<LogOut size={18} />} title="Sair" route="/" />
+        </Links>
+      </Menu>
+
+      <Main>{children}</Main>
+    </Container>
+  );
+}
---
+++ src/components/DashboardTemplate/styles.js
@@ -0,0 +1,78 @@
+import styled from 'styled-components';
+import Expanded from '../Expanded/styles';
+
+const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+
+  @media (max-width: 850px) {
+    flex-direction: column;
+  }
+`;
+
+export const Menu = styled.aside`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  min-width: 300px;
+  padding: 2rem;
+  background-color: ${({ theme }) => theme.primary};
+
+  @media (max-width: 850px) {
+    min-width: 0;
+    padding: 1.5rem;
+  }
+`;
+
+export const Main = styled.main`
+  flex-grow: 1;
+  padding: 3rem 4rem;
+`;
+
+export const Logo = styled.article`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 2rem;
+
+  @media (max-width: 700px) {
+    padding: 1rem;
+  }
+`;
+
+export const Image = styled.img`
+  margin-right: 10px;
+`;
+
+export const Title = styled.h1`
+  color: ${({ theme }) => theme.secondary};
+  font-size: ${({ theme }) => theme.fontSubtitle};
+  font-weight: 600;
+  line-height: 100%;
+
+  @media (max-width: 700px) {
+    font-size: 1.8rem;
+  }
+`;
+
+export const Divider = styled.div`
+  width: 100%;
+  height: 1px;
+  margin: 1rem 0;
+  background-color: rgba(255, 255, 255, 0.2);
+`;
+
+export const Links = styled.ul`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+
+  @media (max-width: 850px) {
+    flex-direction: row;
+
+    & > ${Expanded} { display: none; }
+  }
+`;
+
+export default Container;
