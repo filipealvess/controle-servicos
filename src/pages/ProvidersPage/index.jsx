e1d4ae717367aecbd9b4ed664942c1272cec5f35
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-05-21 11:51:47 -0300

    feat: add empty section component

--- src/pages/ProvidersPage/index.jsx
+++ src/pages/ProvidersPage/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import DashboardHeader from '../../components/DashboardHeader';
 import DashboardTemplate from '../../components/DashboardTemplate';
+import EmptySection from '../../components/EmptySection';
 
 export default function ProvidersPage() {
   return (
     <DashboardTemplate currentPage="providers">
       <DashboardHeader title="Prestadores de Serviço" />
+
+      <EmptySection
+        title="Nenhum prestador cadastrado"
+        description="Para cadastrar seu primeiro prestador, clique no botão NOVO."
+      />
     </DashboardTemplate>
   );
 }
