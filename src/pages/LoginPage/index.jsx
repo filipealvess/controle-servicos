8d91b32989f927a712a9fdcaf2be3dfd723b93cb
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-05-21 08:35:39 -0300

    feat: add auth page template

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,34 +1,16 @@
 import React from 'react';
-import Header from '../../components/Header';
 import Field from '../../components/Field';
-import Tabs from '../../components/Tabs';
-import Container, { Form } from './styles';
 import PrimaryButton from '../../components/PrimaryButton';
 import SizedBox from '../../components/SizedBox';
+import AuthPageTemplate from '../../components/AuthPageTemplate';
 
 export default function LoginPage() {
   return (
-    <Container>
-      <Header />
-      <Tabs />
-
-      <Form>
-        <Field
-          label="E-mail"
-          placeholder="Seu e-mail"
-          type="email"
-        />
-
-        <Field
-          label="Senha"
-          placeholder="******"
-          type="password"
-        />
-
-        <SizedBox height={40} />
-
-        <PrimaryButton text="Entrar" />
-      </Form>
-    </Container>
+    <AuthPageTemplate>
+      <Field label="E-mail" placeholder="Seu e-mail" type="email" />
+      <Field label="Senha" placeholder="******" type="password" />
+      <SizedBox height={40} />
+      <PrimaryButton text="Entrar" />
+    </AuthPageTemplate>
   );
 }
