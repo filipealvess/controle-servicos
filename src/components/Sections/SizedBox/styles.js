5af804433b630423388fb0821420c1958b2b3924
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-05-21 14:09:27 -0300

    feat: improve sized box component with maxWidth and auto attributes

--- src/components/Sections/SizedBox/styles.js
+++ src/components/Sections/SizedBox/styles.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  width: ${({ width }) => `${width}px`};
-  height: ${({ height }) => `${height}px`};
+  width: ${({ width }) => width ? `${width}px` : 'auto'};
+  max-width: ${({ maxWidth }) => maxWidth ? `${maxWidth}px` : 'auto'};
+  height: ${({ height }) => height ? `${height}px` : 'auto'};
+  margin: ${({ auto }) => auto ? '0 auto' : null};
 `;
 
 export default Container;
