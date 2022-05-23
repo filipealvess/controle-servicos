c49187d34498e38d9832edce43923eaddfb50d80
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-05-23 16:11:39 -0300

    feat: list searched providers

--- src/pages/ProvidersPage/index.jsx
+++ src/pages/ProvidersPage/index.jsx
@@ -6,6 +6,8 @@ import AlertPopup from '../../components/Popups/AlertPopup';
 import { listProviders } from '../../controllers/providerController';
 import { useAuth } from '../../context/AuthContext';
 import ProvidersGrid from '../../components/Sections/ProvidersGrid';
+import Pagination from '../../components/Sections/Pagination';
+import SearchResult from '../../components/Links/SearchResult';
 
 export default function ProvidersPage() {
   const [errorPopupIsVisible, setErrorPopupIsVisible] = useState(false);
@@ -13,6 +15,7 @@ export default function ProvidersPage() {
   const [providers, setProviders] = useState([]);
   const [pagesNumber, setPagesNumber] = useState(null);
   const [currentPageNumber, setCurrentPageNumber] = useState(null);
+  const [search, setSearch] = useState('');
   const { user } = useAuth();
 
   useEffect(() => {
@@ -26,8 +29,8 @@ export default function ProvidersPage() {
     user && fetchProviders();
   }, [user]);
 
-  async function fetchProviders(page = 1) {
-    const response = await listProviders(user.id, page);
+  async function fetchProviders(page = 1, search = '') {
+    const response = await listProviders(user.id, page, search);
 
     if (response) {
       const { pages, currentPage, data } = response;
@@ -46,23 +49,51 @@ export default function ProvidersPage() {
     }
   }
 
+  function handleSearch(searchText) {
+    const defaultPage = 1;
+
+    setSearch(searchText);
+    fetchProviders(defaultPage, searchText);
+  }
+
+  function clearSearch() {
+    setSearch('');
+    fetchProviders();
+  }
+
   return (
     <DashboardTemplate currentPage="providers">
-      <DashboardHeader title="Prestadores de Serviço" route="/prestadores/novo" />
+      <DashboardHeader
+        title="Prestadores de Serviço"
+        route="/prestadores/novo"
+        onSearch={handleSearch}
+      />
 
-      {providers.length === 0 && (
+      {providers.length > 0 && (
+        <Pagination
+          currentPage={currentPageNumber}
+          pages={pagesNumber}
+          onChange={handlePageChange}
+        />
+      )}
+
+      {search.length > 0 && <SearchResult onClear={clearSearch} />}
+
+      {providers.length === 0 && search.length === 0 && (
         <EmptySection
           title="Nenhum prestador cadastrado"
           description="Para cadastrar seu primeiro prestador, clique no botão NOVO."
         />
       )}
 
-      <ProvidersGrid
-        providers={providers}
-        currentPage={currentPageNumber}
-        pages={pagesNumber}
-        onChangePage={handlePageChange}
-      />
+      {providers.length > 0 && providers.filter(({ isVisible }) => isVisible).length === 0 && (
+        <EmptySection
+          title="Nenhum serviço encontrado"
+          description="Não foi possível encontrar serviços com a busca realizada"
+        />
+      )}
+
+      <ProvidersGrid providers={providers} />
 
       <AlertPopup
         title="Erro na listagem"
