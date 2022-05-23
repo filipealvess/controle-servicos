067c593739b784de73bf6ac29281b0f71012ba7d
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-05-22 21:54:25 -0300

    feat(SearchBar): add onSearch attribute

--- src/components/Inputs/SearchBar/index.jsx
+++ src/components/Inputs/SearchBar/index.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Search as SearchIcon } from 'react-feather';
 import Container, { Button, Input } from './styles';
 
-export default function SearchBar() {
+export default function SearchBar({ onSearch = () => {} }) {
+  const [search, setSearch] = useState('');
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    onSearch(search);
+  }
+
   return (
-    <Container>
-      <Input placeholder="Pesquisar" />
-      <Button type="button">
+    <Container onSubmit={handleFormSubmit}>
+      <Input
+        placeholder="Pesquisar"
+        value={search}
+        onChange={({ target }) => setSearch(target.value)}
+      />
+
+      <Button>
         <SearchIcon size={18} color="#FFFFFF" />
       </Button>
     </Container>
