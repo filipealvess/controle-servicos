2741d62b853b48fdbf602beacd5bb7ddd4feead5
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-05-23 09:59:24 -0300

    feat: select provider image

--- src/pages/NewProviderPage/index.jsx
+++ src/pages/NewProviderPage/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import DashboardHeader from '../../components/Sections/DashboardHeader';
 import DashboardTemplate from '../../components/Templates/DashboardTemplate';
 import AvatarImageUpload from '../../components/Buttons/AvatarImageUpload';
@@ -7,8 +7,34 @@ import Field from '../../components/Inputs/Field';
 import PrimaryButton from '../../components/Buttons/PrimaryButton';
 import ServicesList from '../../components/Sections/ServicesList';
 import SizedBox from '../../components/Sections/SizedBox';
+import { formatPhone } from '../../controllers/providerController';
 
 export default function NewProviderPage() {
+  const [buttonIsActive, setButtonIsActive] = useState(false);
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [imageSource, setImageSource] = useState('');
+
+  useEffect(() => {
+    const nameIsNotEmpty = name.length > 0;
+    const phoneIsNotEmpty = phone.length > 0;
+    const emailIsNotEmpty = email.length > 0;
+    const hasSelectedImage = imageSource.length > 0;
+
+    setButtonIsActive(nameIsNotEmpty && phoneIsNotEmpty && emailIsNotEmpty && hasSelectedImage);
+  }, [name, phone, email, imageSource]);
+
+  function handleImageSelect({ target }) {
+    const image = target.files[0];
+    const reader = new FileReader();
+
+    reader.readAsDataURL(image);
+    reader.onload = ({ target }) => {
+      setImageSource(target.result);
+    };
+  }
+
   return (
     <DashboardTemplate currentPage="providers">
       <DashboardHeader
@@ -18,13 +44,39 @@ export default function NewProviderPage() {
       />
 
       <Form>
-        <AvatarImageUpload />
-        <Field label="Nome" placeholder="Nome do prestador" required />
-        <Field label="Telefone" placeholder="(00) 0000-0000" type="tel" required />
-        <Field label="E-mail" placeholder="E-mail do prestador" type="email" required />
+        <AvatarImageUpload onSelect={handleImageSelect} src={imageSource} />
+
+        <Field
+          label="Nome"
+          placeholder="Nome do prestador"
+          required
+          value={name}
+          onChange={({ target }) => setName(target.value)}
+        />
+
+        <Field
+          label="Telefone"
+          placeholder="(00) 00000-0000"
+          type="tel"
+          required
+          value={phone}
+          onChange={({ target }) => setPhone(formatPhone(target.value))}
+        />
+
+        <Field
+          label="E-mail"
+          placeholder="E-mail do prestador"
+          type="email"
+          required
+          value={email}
+          onChange={({ target }) => setEmail(target.value)}
+        />
+
         <ServicesList />
+
         <SizedBox height={40} />
-        <PrimaryButton text="Salvar" />
+
+        <PrimaryButton text="Salvar" disabled={!buttonIsActive} />
       </Form>
     </DashboardTemplate>
   );
